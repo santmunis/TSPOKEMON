5f624b5fe49ff3d9b773916d596314dd1db9e70a
Nathan Munis <nathanmunis.signal@gmail.com>
2020-05-21 00:12:24 -0300

    Type Script

---
+++ src/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'react-app',
+      'airbnb',
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['react', 'import', 'jsx-a11y'],
+    rules: {
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['.tsx'],
+        },
+      ],
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-member-accessibility': 'off',
+      "import/extensions": ["error", "ignorePackages", {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never",
+        "mjs": "never"
+      }],
+      "linebreak-style": 0,
+      "code": 100
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+  };
